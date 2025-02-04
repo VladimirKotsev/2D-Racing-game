9e7e938a34c39cc6bdb86cc1a5786786b72c17af
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-02-04 21:21:43 +0200

    Clean start

--- src/constants.py
+++
@@ -1,7 +0,0 @@
-SCREEN_WIDTH = 1380
-SCREEN_HEIGHT = 720
-MIDDLE_SEPARATOR = SCREEN_WIDTH / 2
-MAP = '../assets/images/Test_track2.jpg'
-CAR = '../assets/images/Players_car.png'
-ZOOM_LEVEL = 3
-BLACK = (0, 0, 0)
