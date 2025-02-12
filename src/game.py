7221c6334e16758879b288a3d06e1881bb41dce8
Vladimir Kotsev <v.kotsev04@gmail.com>
2025-02-12 15:28:22 +0200

    Update game.py
    Refactor code to work with singleton Menu

--- src/game.py
+++ src/game.py
@@ -5,6 +5,7 @@ from physics import *
 # Display player's name while playing
 
 pygame.init()
+from menu import Menu
 from utils import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -13,19 +14,7 @@ pygame.display.set_caption(GAME_NAME)
 
 clock = pygame.time.Clock()
 
-background = pygame.image.load(BACKGROUND_IMAGE_PATH).convert()
-background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
-background.set_alpha(10)
-
-player1_input = TextInput(
-    INPUT1_X,
-    INPUT1_PLACEHOLDER
-)
-
-player2_input = TextInput(
-    INPUT2_X,
-    INPUT2_PLACEHOLDER
-)
+menu = Menu()
 
 track = Track()
 
@@ -46,16 +35,9 @@ car2 = Car(track.p2_start[0], track.p2_start[1], track.angular_velocity, BLUE, {
 player1_pov = pygame.Rect(0, 0, SPLIT_WIDTH, SCREEN_HEIGHT)
 player2_pov = pygame.Rect(SPLIT_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
-player1 = Player(player1_input, car1, player1_pov)
-player2 = Player(player2_input, car2, player2_pov)
-
-# Create button with image
-play_button = Button(
-    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2,
-    SCREEN_HEIGHT // 2 - BUTTON_HEIGHT // 2,
-    BUTTON_WIDTH,
-    BUTTON_HEIGHT,
-)
+player1_name, player2_name = menu.get_player_names()
+player1 = Player(player1_name, car1, player1_pov)
+player2 = Player(player2_name, car2, player2_pov)
 
 current_state = 1
 
@@ -67,33 +49,29 @@ while True:
             sys.exit()
 
         if current_state == MENU:
-            player1_input.handle_event(event)
-            player2_input.handle_event(event)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if current_state == MENU and play_button.is_clicked(event.pos):
+            if menu.handle_events(event):
+                # Play button is clicked
                 current_state = COUNTDOWN
-            if current_state == END_GAME:
-                current_state = MENU
-                player1.rematch()
-                player2.rematch()
-                pygame.display.update()
+        elif event.type == pygame.MOUSEBUTTONDOWN and current_state == END_GAME:
+            current_state = MENU
+            player1.rematch()
+            player2.rematch()
+            pygame.display.update()
 
     if player1.is_winner():
-        display_winner(screen, background, player1)
+        menu.display_winner(screen, player1)
         current_state = END_GAME
     if player2.is_winner():
-        display_winner(screen, background, player2)
+        menu.display_winner(screen, player2)
         current_state = END_GAME
 
     if current_state == MENU:
-        screen.blit(background, (0, 0))
-        player1_input.draw(screen)
-        player2_input.draw(screen)
-        play_button.update(mouse_pos)
-        play_button.draw(screen)
+        menu.update(mouse_pos)
+        menu.draw(screen)
     elif current_state == COUNTDOWN:
-        run_countdown(screen, background)
+        menu.run_countdown(screen)
         current_state = GAME
+        player1.nickname, player2.nickname = menu.get_player_names()
     elif current_state == GAME:
         player1.check_collision(player2)
 
